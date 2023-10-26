97b9c09a9cdb1116db2bb51b72d968855da0871a
Duduar Deagle <duduar@skiff.com>
2023-10-25 22:23:37 -0600

    💚 Fix URL base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 
 export default defineConfig({
 	site: "https://duduardeagle.github.io",
-	base: "/chirp-landing/",
+	base: "/chirp-landing",
 	build: {
 		assets: "assets"
 	}
